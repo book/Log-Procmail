29d1f6f1d0f40b7b048322372074ffbfbd57c4c6
Philippe "BooK" Bruhat <book@cpan.org>
2004-04-18 00:05:06 +0000

    Modified the log reading routine to handle broken log files just like mailstat does (see t/procmail4.log for a real life example).
    This mostly means that logs for which we can't find a folder are ignored.

--- Procmail.pm
+++ Procmail.pm
@@ -60,6 +60,7 @@ sub new {
         fh     => new IO::File,
         files  => [@_],
         errors => 0,
+        buffer => [],
     }, $class;
 }
 
@@ -86,7 +87,7 @@ Some informational messages can be put by procmail(1) in the log file.
 If the C<errors> attribute is true, these lines are returned one at a time.
 
 With errors enabled, you have to check that next() actually returns a
-Log::Procmail::Abstract object. Here's is an example:
+Log::Procmail::Abstract object. Here is an example:
 
     $log->errors(1);
 
@@ -96,11 +97,11 @@ Log::Procmail::Abstract object. Here's is an example:
         # if it's an error line
         if ( !ref $rec ) {
             # this is not a log, but an informational message
-            ...
+            # do something with it
             next;
         }
 
-        ...
+        # normal log processing
     }
 
 =cut
@@ -112,40 +113,54 @@ sub next {
     unless ( $log->{fh}->opened ) {
         if ( @{ $log->{files} } ) {
             my $file = shift @{ $log->{files} };
-            $log->open($file);
+            $log->_open($file);
         }
         else { return }
     }
 
     # try to read a record (3 lines)
     my $fh  = $log->{fh};
-    my $rec = Log::Procmail::Abstract->new;
-    my $read;
-    READ: {
+  READ:
+    {
+        my $read;
         while (<$fh>) {
             $read++;
 
             # should carp if doesn't get what's expected
             # (From, then Subject, then Folder)
+
+            # From create a new Abstract
             /^From\s+(\S+)\s+(.*)/ && do {
+                push @{$log->{buffer}}, Log::Procmail::Abstract->new;
 
                 # assert: $read == 1;
-                $rec->from($1);
-                $rec->date($2);
+                $log->{buffer}[-1]->from($1);
+                $log->{buffer}[-1]->date($2);
+
+                # return ASAP
+                last READ if @{$log->{buffer}} > 1;
                 next;
             };
 
             # assert: $read == 2;
-            /^ Subject: (.*)/i && do { $rec->subject($1); next; };
+            /^ Subject: (.*)/i && do {
+                push @{$log->{buffer}}, Log::Procmail::Abstract->new
+                    unless @{$log->{buffer}};
+                $log->{buffer}[0]->subject($1);
+                next;
+            };
 
             # procmail tabulates with tabs and spaces... :-(
             # assert: $read == 3;
+            # Folder means the end of this record
             /^  Folder: (.*?)\s+(\d+)$/ && do {
+                push @{$log->{buffer}}, Log::Procmail::Abstract->new
+                  unless @{$log->{buffer}};
 
                 # assert: $read == 3;
-                $rec->folder($1);
-                $rec->size($2);
-                last;
+                $log->{buffer}[0]->folder($1);
+                $log->{buffer}[0]->size($2);
+                last READ;
             };
 
             # fall through: some error message
@@ -154,18 +169,21 @@ sub next {
 
             # or return it?
             chomp;
-            $rec = $_;
+            push @{$log->{buffer}}, $_;
             last;
         }
 
         # in case we couldn't read the first line
-        if ( !$read ) {
+        if ( !$read or @{$log->{buffer}} == 0 ) {
+
+            # return ASAP
+            last READ if @{$log->{buffer}};
 
             # go to next file
             if ( @{ $log->{files} } ) {
                 $fh->close;
                 my $file = shift @{ $log->{files} };
-                $log->open($file);
+                $log->_open($file);
                 redo READ;
             }
 
@@ -173,7 +191,16 @@ sub next {
             else { return }
         }
     }
-    return $rec;
+
+    # we have an abstract
+    my $rec = shift @{$log->{buffer}};
+    if($rec->isa( 'Log::Procmail::Abstract')) {
+        # the folder field is required
+        goto READ unless defined $rec->folder;
+        $rec->{source} = $log->{source};
+    }
+
+    return $rec
 }
 
 =item $log->push( $file [, $file2 ...] );
@@ -181,7 +208,7 @@ sub next {
 Push one or more files on top of the list of log files to examine.
 When Log::Procmail runs out of abstracts to return (i.e. it reaches the
 end of the file), it transparently opens the next file (if there is one)
-and keeps returning abstracts.
+and keeps returning new abstracts.
 
 =cut
 
@@ -208,7 +235,7 @@ sub errors {
 # *internal method*
 # opens a file or replace the old filehandle by the new one
 # push() can therefore accept refs to typeglobs, IO::Handle, or filenames
-sub open {
+sub _open {
     my ( $log, $file ) = @_;
     if ( ref $file eq 'GLOB' ) {
         $log->{fh} = *$file{IO};
@@ -220,6 +247,7 @@ sub open {
     else {
         $log->{fh}->open($file) or carp "Can't open $file: $!";
     }
+    $log->{source} = $file;
 }
 
 sub DESTROY {
@@ -262,9 +290,12 @@ otherwise.
     # count mail received per folder
     while( $rec = $log->next ) { $folder{ $rec->folder }++ }
 
+The source() accessor returns the name of the log file or the string
+representation of the handle, if a filehandle was given.
+
 =cut
 
-for my $attr (qw( from date subject size folder ) ) {
+for my $attr (qw( from date subject size folder source ) ) {
     no strict 'refs';
     *{"Log::Procmail::Abstract::$attr"} = sub {
         my $self = shift;
@@ -283,7 +314,9 @@ sub ymd {
     my $self = shift;
     croak("Log::Procmail::Abstract::ymd cannot be used to set the date")
       if @_;
-    $self->{date} =~ /^... (...) (..) (..):(..):(..) .*(\d\d\d\d)$/;
+    return undef unless defined $self->{date};
+    $self->{date} =~ /^... (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) ([ \d]\d) (\d\d):(\d\d):(\d\d) .*(\d\d\d\d)$/;
+    return undef unless $1;
     return sprintf( "%04d%02d%02d$3$4$5", $6, $month{$1} + 1, $2 );
 }
 
@@ -295,7 +328,9 @@ The Log::Procmail object should be able to read from STDIN.
 
 =head1 BUGS
 
-The ymd() method should be smarter.
+Sometimes procmail(1) logs are mixed up. When this happens, I've chosen
+to accept them the way mailstat(1) does: they are discarded unless they
+have a C<Folder> line.
 
 Please report all bugs through the rt.cpan.org interface:
 
@@ -316,7 +351,10 @@ available for Log::Procmail and many other projects.
 
 =head1 COPYRIGHT 
 
-Copyright (c) 2002, Philippe Bruhat. All Rights Reserved.
+Copyright (c) 2002-2004, Philippe Bruhat. All Rights Reserved.
+
+=head1 LICENSE
+
 This module is free software. It may be used, redistributed
 and/or modified under the terms of the Perl Artistic License
 (see http://www.perl.com/perl/misc/Artistic.html)
